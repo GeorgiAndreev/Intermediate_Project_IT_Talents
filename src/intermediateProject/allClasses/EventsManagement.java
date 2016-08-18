de13503cfa6b0bd5e1127db6530ab38339ee93b5
GeorgiAndreev <georgi_andreev@yahoo.com>
2016-08-19 00:19:13 +0300

    added Demo class

---
+++ src/intermediateProject/allClasses/EventsManagement.java
@@ -0,0 +1,22 @@
+package intermediateProject.allClasses;
+
+import java.io.*;
+import java.util.*;
+
+public class EventsManagement {
+
+	private HashMap<String, Event> allEventsInCenter;
+
+	public void addEventToCenter(Event newEvent) {
+		throw new UnsupportedOperationException("The method is not implemented yet.");
+	}
+
+	public void removeEventFromCenter(Event eventToRemove) {
+		throw new UnsupportedOperationException("The method is not implemented yet.");
+	}
+
+	public void sellPlaceForEvent(LoggedAccount participant, Event event) {
+		throw new UnsupportedOperationException("The method is not implemented yet.");
+	}
+
+}
